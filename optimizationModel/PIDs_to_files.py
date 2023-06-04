68a3f6cf3a5fe5524571e9a71daf252169004f37
Michelle Lam <110070274+michellelam777@users.noreply.github.com>
2023-06-04 16:23:43 -0700

    adding script that reformats SAM output for opt
    script converts SAM simulation output so that there is one file per PID and each file contains 3 years of data

---
+++ optimizationModel/PIDs_to_files.py
@@ -0,0 +1,54 @@
+### -----------------------
+# Loading packages & defaults
+### -----------------------
+import pandas as pd
+import os
+import time as time
+
+### -----------------------
+# Reading in the yearly data & setting folder path
+### -----------------------
+# Load data from both years
+df_cf_2012 = pd.read_csv('/Users/avidalmeza/Downloads/wind/capacityFactor_2012/us_PID_cords_2012.csv')
+df_cf_2013 = pd.read_csv('/Users/avidalmeza/Downloads/wind/capacityFactor_2013/us_PID_cords_2013.csv')
+df_cf_2014 = pd.read_csv('/Users/avidalmeza/Downloads/wind/capacityFactor_2014/us_PID_cords_2014.csv')
+
+# Define the folder where the output files will be saved
+folder = '/Users/avidalmeza/Downloads/wind_capacityFactor_filePerPID'
+
+### -----------------------
+# Reshaping the data & reading it as a csv for each FID
+### -----------------------
+
+# initializing the counter variable
+counter = 0
+
+# Loop through each row of the dataframe and extract data for that location
+for i in range(df_cf_2012.shape[0]):
+    location_data_2012 = df_cf_2012.iloc[i, 5:].values.tolist()
+    location_data_2013 = df_cf_2013.iloc[i, 5:].values.tolist()
+    location_data_2014 = df_cf_2014.iloc[i, 5:].values.tolist()  # add this line
+    location_name = df_cf_2012.iloc[i, 0]
+    
+    # Create a new dataframe containing the hourly energy data for both years
+    location_df_2012 = pd.DataFrame({'energy_generation': location_data_2012})
+    location_df_2013 = pd.DataFrame({'energy_generation': location_data_2013})
+    location_df_2014 = pd.DataFrame({'energy_generation': location_data_2014})  # add this line
+
+    # adding a new column with the hour and year index
+    location_df_2012['hour'] = range(1, 8761)
+    location_df_2013['hour'] = range(8761, 17521)
+    location_df_2014['hour'] = range(17521, 26281)  # add this line
+
+    # Concatenate the dataframes and sort by hour
+    location_df = pd.concat([location_df_2012, location_df_2013, location_df_2014]).sort_values('hour')  # update this line
+
+    # Save the new dataframe as a CSV file with the location name as the file name
+    location_df.to_csv(f'{folder}/capacity_factor_PID{location_name}.csv', index = False)
+    
+    counter += 1
+    # increment the counter and print an update statement every 200 files saved
+    if counter % 200 == 0:
+        print(f"Saved files for the PID {counter}.")
+
+
